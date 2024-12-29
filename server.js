df54ca9fb03c52b8559e213a8d9c8f384b7f44c3
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2024-12-30 00:53:49 +1100

    Initial setup

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
